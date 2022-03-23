1b8c95fbf4adb4ab0085d9dec6613e77731fed33
Shubham Chauhan <cshubham1911@gmail.com>
2022-03-23 17:46:30 +0530

    first commit

---
+++ src/FlashCard.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function FlashCard() {
+  const [flip, setFlip] = useState(false)
+  return (
+    <div onClick>
+      {}
+    </div>
+  )
+}
+
+export default FlashCard
